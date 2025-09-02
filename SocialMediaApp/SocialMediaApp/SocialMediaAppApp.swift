b2c3cc7eab183031e5334009a6120c6f43919666
Asad <asad.mobiledev@gmail.com>
2025-09-02 11:09:08 +0500

    initial project setup

---
+++ SocialMediaApp/SocialMediaApp/SocialMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaAppApp.swift
+//  SocialMediaApp
+//
+//  Created by Asad Mehmood on 02/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
