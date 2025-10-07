fc3e7d1aee7154657ee544e06c671ae2e28d4d15
Asad <asad.mobiledev@gmail.com>
2025-10-07 10:18:56 +0500

    Added test

--- SocialMediaApp/SocialMediaAppTests/SocialMediaAppTests.swift
+++ SocialMediaApp/SocialMediaAppTests/SocialMediaAppTests.swift
@@ -6,11 +6,13 @@
 //
 
 import Testing
+@testable import SocialMediaApp
 
 struct SocialMediaAppTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    
+    @Test func globalFunc() {
+        let appText = AppText.OK
+        let expectMetaData = "OK"
+        #expect(appText == expectMetaData)
     }
-
 }
